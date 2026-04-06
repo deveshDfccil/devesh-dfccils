ba422cbca74330d45658a93e100583f5e9cc7214
deveshCetpa <deveshparatap@cetpainfotech.com>
2026-04-06 13:01:23 +0530

    frist COmmit

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@
 // const GlobalStyles = () => (
 //   <style>{`
 //     @import url('https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=DM+Sans:ital,wght@0,300;0,400;0,500;1,300&family=JetBrains+Mono:wght@300;400;500&display=swap');
-
 //     *, *::before, *::after { box-sizing: border-box; margin: 0; padding: 0; }
 //     html { scroll-behavior: smooth; }
 //     body { font-family: 'DM Sans', sans-serif; background: #030712; color: white; overflow-x: hidden; }
